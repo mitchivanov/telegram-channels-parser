d1ac6ae29763f95549452ef068e01d64fcfeab28
Michael Ivanov <hackermanmitch@gmail.com>
2025-06-14 21:18:58 +0400

    12

--- filter/db.py
+++ filter/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-POSTGRES_DSN = os.environ.get("POSTGRES_DSN", "postgresql+asyncpg://postgres:postgres@postgres:5432/filter")
+POSTGRES_DSN = os.environ.get("POSTGRES_DSN", "postgresql+asyncpg://postgres:postgres@db:5432/filter")
 
 engine = create_async_engine(POSTGRES_DSN, echo=True, future=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
